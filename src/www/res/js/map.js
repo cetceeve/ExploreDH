d31bf7886c57978f3a0c7a22ec64319a8c44fbb3
kindOfCurly <marie.sautmann@gmx.de>
2019-05-17 21:57:06 +0200

    feat/#10: basic map
    Added a basic version of a map (openlayers).

---
+++ src/www/res/js/map.js
@@ -0,0 +1,21 @@
+/** 
+ * MapViewController.
+ * 
+ * At the moment used to create a basic map.
+ * TODO: convert to nice module structure.
+ */
+
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([37.41, 8.82]),
+      zoom: 4
+    })
+  });
+
+ 
