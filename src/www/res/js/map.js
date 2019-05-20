f1b85816e461f761c85610f75dc667baff51b523
kindOfCurly <marie.sautmann@gmx.de>
2019-05-20 13:02:54 +0200

    feat/#10: basic map
    Formatting added.

--- src/www/res/js/map.js
+++ src/www/res/js/map.js
@@ -4,38 +4,37 @@
  * At the moment used to create/visualize a basic map.
  */
 
- class Map {
-   constructor() {
-     //super(); -- throws error: "SyntaxError: super() is only valid in derived class constructors"
-     this.initMap();
-   }
- 
+class Map {
+  constructor() {
+    //super(); -- throws error: "SyntaxError: super() is only valid in derived class constructors"
+    this.initMap();
+  }
 
-initMap() {
-  var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.Stamen({
+
+  initMap() {
+    var map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.Stamen({
             layer: "toner"
+          })
         })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([37.41, 8.82]),
+        zoom: 4
       })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([37.41, 8.82]),
-      zoom: 4
-    })
-  });
+    });
+  }
 }
- }
 
- export default new Map();
-
+export default new Map();
 
 
 
 
 
-   
 
- 
+
+
