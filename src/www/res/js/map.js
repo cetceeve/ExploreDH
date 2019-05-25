b2c6a8e1a745603ec79ddd47613f298c2b9ef738
kindOfCurly <marie.sautmann@gmx.de>
2019-05-25 22:16:40 +0200

    feat/#18: map elements
    code optimization

--- src/www/res/js/map.js
+++ src/www/res/js/map.js
@@ -30,38 +30,20 @@ class Map {
       })
     });
 
-    ////////////////// add point+line
+    ////////////////// add point
 
-    var point_feature = new ol.Feature({});
-    var point_geom = new ol.geom.Point(
-      [20, 20]
-    );
-    point_feature.setGeometry(point_geom);
-    var linestring_feature = new ol.Feature({
-      geometry: new ol.geom.LineString(
-        [[10, 20], [20, 10], [30, 20]]
-      )
+    var point_feature = new ol.Feature({
+      geometry: new ol.geom.Point([13.41, 52.52])
     });
+
     var vector_layer = new ol.layer.Vector({
       source: new ol.source.Vector({
-        features: [point_feature, linestring_feature]
+        features: [point_feature]
       })
     })
     map.addLayer(vector_layer);
 
-    var features = [
-      point_feature,
-      linestring_feature
-    ];
-
-    function transform_geometry(element) {
-      var current_projection = new ol.proj.Projection({ code: "EPSG:4326" });
-      var new_projection = layer.getSource().getProjection();
-
-      element.getGeometry().transform(current_projection, new_projection);
-    }
-
-    features.forEach(transform_geometry);
+    point_feature.getGeometry().transform(new ol.proj.Projection({ code: "EPSG:4326" }), layer.getSource().getProjection());
 
     var fill = new ol.style.Fill({
       color: [180, 0, 0, 0.3]
