c315d838f1c2fd1c9ba780454a1aa29fb5b3510b
Fabian Zeiher <fzeiher@gmail.com>
2019-05-26 13:48:50 +0200

    feat/#7: get all filenames

---
+++ src/scripts/parser_base.py
@@ -0,0 +1,13 @@
+import os
+
+dir = "../../TEI"
+
+def readFiles():
+    with os.scandir(dir) as it:
+        for entry in it:
+            if not entry.name.startswith('.') and entry.name.endswith('.xml') and entry.is_file():
+                print(entry.name)
+                # parse xml file here
+
+if __name__ == "__main__":
+    readFiles()
