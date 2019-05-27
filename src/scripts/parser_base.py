63f9aed97f8ad3b135a47288d47e575f9a59aab2
Fabian Zeiher <fzeiher@gmail.com>
2019-05-27 15:30:20 +0200

    feat/#7: json for additional entities

--- src/scripts/parser_base.py
+++ src/scripts/parser_base.py
@@ -6,6 +6,7 @@ import parser_listorg as parserListOrga
 import novatim_adapter as geocoder
 import spacy_adapter as ner
 
+
 dirTEI = "../../data/TEI"
 pathListPerson = "../../data/preprocessed/listperson.xml"
 pathListOrga = "../../data/preprocessed/listorg.xml"
@@ -17,17 +18,6 @@ def readTEI():
                 print(entry.name)
                 # parse xml file here
 
-if __name__ == "__main__":
-    # readTEI()
-
-    dictPerson = parserListPerson.parse(pathListPerson)
-    dictOrga, dictLocation = parserListOrga.parse(pathListOrga)
-
-    # ner.runNER("Language Technology Group, Universität Hamburg, Deutschland")
-    # geocoder.getLocation("Universität Rostock")
-
-    # writeMissingEntityInfoFile(dictPerson, dictOrga)
-    
 
 def writeMissingEntityInfoFile(dictPerson, dictOrga):
     file = open("dhd2019_missing_info.txt", mode="w", encoding="utf-8")
@@ -45,3 +35,58 @@ def writeMissingEntityInfoFile(dictPerson, dictOrga):
             file.write("\n" + json.dumps(orga, indent=4, ensure_ascii=False))
 
     file.close()
+
+
+def writeAdditionalEntityJSONFile(dictPerson, dictOrga):
+    with open("dhd2019_additional_entities.json", mode="w", encoding="utf-8") as file:
+        peopleMissingOrgas = []
+        additionalOrgas = []
+        orgasMissingLocations = []
+        res = {
+            "peopleMissingOrgas": peopleMissingOrgas,
+            "additionalOrgas": additionalOrgas,
+            "orgasMissingLocations": orgasMissingLocations
+        }
+
+        # get people with no associated organisation
+        counter = 148
+        for person in dictPerson.values():
+            if "__temp__affil" in person:
+                person["organisation"] = "org__" + str(counter)
+                counter += 1
+
+                peopleMissingOrgas.append(person)
+
+        # create new organisations
+        for i in range(4):
+            additionalOrgas.append({
+                "id": "org__" + str(148 + i),
+                "name": "{{enter useful organisation name e.g. Germanisches Nationalmuseum, Nürnberg }}",
+                "locationame": "{{enter city name here (will be processed by novatim) z.B. Nürnberg}}"
+            })
+
+        # get orgas with no location
+        for orga in dictOrga.values():
+            if "location" not in orga:
+                orgasMissingLocations.append({
+                    "id": orga["id"],
+                    "name": orga["name"],
+                    "city": "{{enter city name here (will be processed by novatim)  z.B. Würzburg}}"
+                })
+        
+        file.write(json.dumps(res, indent=4, ensure_ascii=False))
+
+
+if __name__ == "__main__":
+    # readTEI()
+
+    dictPerson = parserListPerson.parse(pathListPerson)
+    dictOrga, dictLocation = parserListOrga.parse(pathListOrga)
+
+
+    # ner.runNER("Language Technology Group, Universität Hamburg, Deutschland")
+    # geocoder.getLocation("Universität Rostock")
+
+    # print(json.dumps(dictPerson, indent=4, ensure_ascii=False))
+    # writeMissingEntityInfoFile(dictPerson, dictOrga)
+    writeAdditionalEntityJSONFile(dictPerson, dictOrga)
