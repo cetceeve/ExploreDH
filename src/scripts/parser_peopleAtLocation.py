90f89f381228c15d28cfdb1cb00851d8ef7c74ff
Fabian Zeiher <fzeiher@gmail.com>
2019-05-28 18:30:05 +0200

    refact/#7: optimize peopleAtLocation script

--- src/scripts/parser_peopleAtLocation.py
+++ src/scripts/parser_peopleAtLocation.py
@@ -6,7 +6,6 @@ def printPeopleAtLocation(dictPerson, dictOrga, dictLocation):
     # deepcopy dictLocation with new params
     for loc in dictLocation.values():
         dictRes[loc["id"]] = {
-            "id": loc["id"],
             "lat": loc["lat"],
             "lon": loc["lon"],
             "numOfPeople": 0
@@ -14,19 +13,11 @@ def printPeopleAtLocation(dictPerson, dictOrga, dictLocation):
 
     # count people per location
     for person in dictPerson.values():
-        dictRes[dictOrga[person["orga"]]["location"]]["numOfPeople"] += 1
-        
+        dictRes[dictOrga[person["orga"]]["location"]]["numOfPeople"] += 1        
         # in long form:
         # orga = dictOrga[person["orga"]]     
         # location = orga["location"]
         # resEntry = dictRes["location"]
         # resEntry["numOfPeople"] += 1
 
-    # convert to array
-    resArray = []
-    for datapoint in dictRes.values():
-        del datapoint["id"]
-        resArray.append(datapoint)
-    
-    # print(json.dumps(resArray, indent=4, ensure_ascii=False))
-    cache.write(resArray, "peopleAtLocation")
+    cache.write(list(dictRes.values()), "peopleAtLocation")
