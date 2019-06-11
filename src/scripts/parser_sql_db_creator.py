3eb5b31b779c3dc22bc5c870b9091c23896d832d
Fabian Zeiher <fzeiher@gmail.com>
2019-06-11 07:52:15 +0200

    feat/#28: article table

--- src/scripts/parser_sql_db_creator.py
+++ src/scripts/parser_sql_db_creator.py
@@ -18,6 +18,7 @@ def create_db(db_file, dictPerson, dictOrga, dictLocation, dictArticle, dictKeyw
     _commitToTable(conn, cursor, "orga", _getValuesAsTupleList(dictOrga))
     _commitToTable(conn, cursor, "person", _getPeopleAsTupleList(dictPerson))
     _commitToTable(conn, cursor, "email", _getEmailsAsTupleList(dictPerson))
+    _commitToTable(conn, cursor, "article", _getArticlesAsTupleList(dictArticle))
     conn.close()
 
 
@@ -28,6 +29,7 @@ def _createTables(conn, curser):
     curser.execute("CREATE TABLE orga (id TEXT PRIMARY KEY, name TEXT, location REFERENCES location(id))")
     curser.execute("CREATE TABLE person (id TEXT PRIMARY KEY, firstname TEXT, lastname TEXT, orga REFERENCES orga(id))")
     curser.execute("CREATE TABLE email (id TEXT PRIMARY KEY, email TEXT, person REFERENCES author(id))")
+    curser.execute("CREATE TABLE article (id TEXT PRIMARY KEY, title TEXT, abstract TEXT)")
     conn.commit()
 
 
@@ -37,6 +39,13 @@ def _getPeopleAsTupleList(dictPerson):
 
 def _getEmailsAsTupleList(dictPerson):
     return [(str(uuid.uuid1()), email, key) for key, person in dictPerson.items() for email in person["email"]]
+
+
+def _getArticlesAsTupleList(dictArticle):
+    return [(article["id"], article["title"], article["abstract"]) for article in dictArticle.values()]
+# {"id": "4ff9ad92-8bb6-11e9-8a21-f46d04af11fe", "title": "VAnnotatoR: Ein Werkzeug zur Annotation multimodaler Netzwerke in dreidimensionalen virtuellen Umgebungen", "abstract": "ASSA!",
+# "keywords": ["4ff9d49c-8bb6-11e9-b565-f46d04af11fe", "4ff9d49d-8bb6-11e9-88c0-f46d04af11fe", "4ff9d49e-8bb6-11e9-a816-f46d04af11fe", "4ff9d49f-8bb6-11e9-ac2e-f46d04af11fe", "4ff9d4a0-8bb6-11e9-837a-f46d04af11fe", "4ff9d4a1-8bb6-11e9-b91e-f46d04af11fe", "4ff9d4a2-8bb6-11e9-a047-f46d04af11fe", "4ff9d4a3-8bb6-11e9-a0c8-f46d04af11fe", "4ff9d4a4-8bb6-11e9-afda-f46d04af11fe", "4ff9d4a5-8bb6-11e9-9fd4-f46d04af11fe", "4ff9d4a6-8bb6-11e9-88ec-f46d04af11fe", "4ff9d4a7-8bb6-11e9-884f-f46d04af11fe"],
+# "authors": ["person__abrami-em-uni-frankfurt-de", "person__s2717197-stud-uni-frankfurt-de", "person__mehler-em-uni-frankfurt-de"]},
 
 
 def _getValuesAsTupleList(dictonary):
