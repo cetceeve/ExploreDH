b7027e28d8e30cc498e27dd10af1fa711267b946
Fabian Zeiher <fzeiher@gmail.com>
2019-07-14 21:31:43 +0200

    refact/#52: improve readability

--- src/index.js
+++ src/index.js
@@ -11,22 +11,21 @@ const express = require("express"), PORT = 5012,
     jsonDB = JSON.parse(fs.readFileSync("../data/output/output_database.json"));
 
 var app = express(),
-    db = new sqlite3.Database("../data/db/dhd_data.db", sqlite3.OPEN_READONLY, (err) => {
-        if (err !== null) {
-            console.error(err);
-        }
-    });
+    db = new sqlite3.Database("../data/db/dhd_data.db", sqlite3.OPEN_READONLY, (err) => { if (err) { console.error(err); } });
 
 app.use(express.static("www"));
 app.listen(PORT);
 console.log("Server runing at http://localhost:" + PORT);
 
+app.get("/connections", function (req, res) {
+    console.log("sending connections upstream");
+    res.json(conns);
+});
+
 app.get("/connections/peoplePerOrga", function (req, res) {
-    console.log(req.url);
     db.all("SELECT orga.lat, orga.lon, orga.name, count(person.id) AS numOfPeople FROM orga INNER JOIN person ON person.orga=orga.id GROUP BY orga.id ORDER BY numOfPeople", function (err, rows) {
-        if (err !== null) {
-            console.error(err);
-        } else {
+        if (err) { console.error(err); }
+        else {
             console.log("sending peoplePerOrga upstream");
             res.json(rows);
         }
@@ -34,11 +33,10 @@ app.get("/connections/peoplePerOrga", function (req, res) {
 });
 
 app.get("/connections/connectionsOnArticle/:articleID", function (req, res) {
-    console.log(req.params);
+    console.log(req.url + req.params);
     db.all("SELECT DISTINCT orga.id, orga.lat, orga.lon FROM orga INNER JOIN person ON person.orga=orga.id INNER JOIN article_person_link AS link ON person.id=link.person_id INNER JOIN article on link.article_id=article.id WHERE article.id=$id", { $id: req.params.arcticleID }, function (err, rows) {
-        if (err !== null) {
-            console.error(err);
-        } else {
+        if (err) { console.error(err); }
+        else {
             // create connections
             let data = [];
             for (let i = 0; i < rows.length; i++) {
@@ -46,18 +44,16 @@ app.get("/connections/connectionsOnArticle/:articleID", function (req, res) {
                     data.push([rows[i], rows[j]]);
                 }
             }
-            console.log("sending data upstream");
+            console.log("sending connectionsOnArticle upstream");
             res.json(data);
         }
     });
 });
 
 app.get("/search", function (req, res) {
-    console.log(req.url);
     db.all("SELECT id, title FROM article", function (err, rows) {
-        if (err !== null) {
-            console.error(err);
-        } else {
+        if (err) { console.error(err); }
+        else {
             console.log("sending search options upstream");
             res.json(rows);
         }
@@ -65,17 +61,16 @@ app.get("/search", function (req, res) {
 });
 
 app.get("/article/:id", function (req, res) {
-    console.log(req.params);
+    console.log(req.url + req.params);
     console.log("sending article upstream");
     res.json(buildArticleForDisplay(req.params.id));
 });
 
 app.get("/article/articleByOrga/:orgaID", function (req, res) {
-    console.log(req.params);
+    console.log(req.url + req.params);
     db.all("SELECT DISTINCT article.id FROM article INNER JOIN article_person_link AS link ON article.id=link.article_id INNER JOIN person on link.person_id=person.id INNER JOIN orga ON person.orga=orga.id WHERE orga.id=$id", { $id: req.params.orgaID }, function (err, rows) {
-        if (err !== null) {
-            console.error(err);
-        } else {
+        if (err) { console.error(err); }
+        else {
             let data = [];
             for (let item of rows) {
                 data.push(buildArticleForDisplay(item.id));
@@ -84,12 +79,6 @@ app.get("/article/articleByOrga/:orgaID", function (req, res) {
             res.json(data);
         }
     });
-});
-
-app.get("/connections", function (req, res) {
-    console.log(req.url);
-    console.log("sending connections upstream");
-    res.json(conns);
 });
 
 function buildArticleForDisplay(articleID) {
